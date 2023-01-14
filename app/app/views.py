bd446a259863f84124d7db1d9baa63a180d5b499
Siponek <szinkowicz@gmail.com>
2023-01-14 12:25:56 +0100

    Adde testing views

--- app/app/views.py
+++ app/app/views.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 from app import app
 
-cors = CORS(app)
+cors = CORS(app=app, resources={r"/api/*": {"origins": "*"}}, vary_header=True, supports_credentials=True)
 # This is for local testing
 app.config.from_pyfile('config/debug_environment.cfg')
 
@@ -19,16 +19,19 @@ app.config.update(
 def main():
     app.run(host='0.0.0.0', port=80, debug=True)
     
-@app.route('/index')
+@app.route('/', strict_slashes=False, methods=['GET'])
+@app.route('/index', strict_slashes=False, methods=['GET'])
 def index():
     return render_template(template_name_or_list='index.html',utc_dc= datetime.utcnow())
+
+@app.route("/web_21", methods=['GET'])
+def web_21():
+    return render_template(template_name_or_list='web_21.html')
 
 @app.route('/get_fifa_data', methods=['GET'])
 def get_fifa_data():
     def wololo():
         return 'wololo'
-    
-
     return jsonify({'data': 'fifa data'})
 
 
--- app/prestart.sh
+++ app/prestart.sh
@@ -1,2 +1,2 @@
-echo pwd
+echo "Say hello to flask-NGINX-Docker web app! Don't forget, don't do drugs and don't drink, and drive!"
 ls -la
