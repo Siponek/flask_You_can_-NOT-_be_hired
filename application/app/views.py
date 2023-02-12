0913434efca0d96922fac312b87c00ec56128b96
Siponek <szinkowicz@gmail.com>
2023-02-12 20:12:57 +0100

    Trying to get rid of logging in flask

--- application/app/views.py
+++ application/app/views.py
@@ -1,39 +1,85 @@
-from flask import Flask
-from flask import request, jsonify, render_template
-from flask_cors import CORS, cross_origin
 from datetime import datetime
+from os import getenv, environ
+from sys import stderr
+
+from flask import Flask, request, json, jsonify, render_template
+from flask_cors import CORS, cross_origin
+from werkzeug.exceptions import HTTPException
 
 from app import app
 
-cors = CORS(app=app, resources={r"/api/*": {"origins": "*"}}, vary_header=True, supports_credentials=True)
-# This is for local testing
-app.config.from_pyfile('config/debug_environment.cfg')
 
-# This will be for docker-compose
-# app.config.from_envvar('CONFIGURATION_FILE')
-app.config.update(
-        CORS_Headers='Content-Type',
+cors = CORS(
+    app=app,
+    resources={r"/api/*": {"origins": "*"}},
+    vary_header=True,
+    supports_credentials=True,
 )
+# This is for local testing
+
 
 # if run from command line as python flask_host.py
 def main():
-    app.run(host='0.0.0.0', port=80, debug=True)
-    
-@app.route('/', strict_slashes=False, methods=['GET'])
-@app.route('/index', strict_slashes=False, methods=['GET'])
+    app.logger.disabled = True
+    # WSGI server runs on port LISTEN_PORT by default
+    app.run(
+        host="0.0.0.0",
+        debug=True,
+    )
+
+
+# @app.errorhandler(HTTPException)
+# def handle_exception(e):
+#     """Return JSON instead of HTML for HTTP errors."""
+#     # start with the correct headers and status code from the error
+#     response = e.get_response()
+#     # replace the body with JSON
+#     response.data = json.dumps(
+#         {
+#             "code": e.code,
+#             "name": e.name,
+#             "description": e.description,
+#         }
+#     )
+#     response.content_type = "application/json"
+#     return response
+
+
+@app.route("/", strict_slashes=False, methods=["GET"])
+@app.route("/index", strict_slashes=False, methods=["GET"])
 def index():
-    return render_template(template_name_or_list='index.html',utc_dc= datetime.utcnow())
+    return render_template(
+        template_name_or_list="index.html", utc_dc=datetime.utcnow()
+    )
 
-@app.route("/web_21", methods=['GET'])
+
+@app.route("/web_21", methods=["GET"])
 def web_21():
-    return render_template(template_name_or_list='web_21.html')
+    return render_template(template_name_or_list="web_21.html")
 
-@app.route('/get_fifa_data', methods=['GET'])
+
+@app.route("/test", methods=["GET"])
+def test():
+    return render_template(template_name_or_list="test.html")
+
+
+@app.route("/day4", methods=["GET"])
+def task4():
+    return render_template(template_name_or_list="day4.html")
+
+
+@app.route("/day5", methods=["GET"])
+def task5():
+    return render_template(template_name_or_list="day5.html")
+
+
+@app.route("/get_fifa_data", methods=["GET"])
 def get_fifa_data():
     def wololo():
-        return 'wololo'
-    return jsonify({'data': 'fifa data'})
+        return "wololo"
+
+    return jsonify({"data": "fifa data"})
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
